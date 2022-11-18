bb8d3c8ea00e2a2d0a283521334dc1f29b123c47
JOSE CAIRA <26josecaira123@gmail.com>
2022-11-18 11:46:10 -0500

    nueva modificacion

--- Views/home.php
+++ Views/home.php
@@ -5,6 +5,6 @@
   <title>DREP-CEPLAN</title>
 </head>
 <body>
-    <p>Mini Framework planificacion POI V1.1 web - <a href="http://localhost/civilweb/dashboard">www.drep.dgi.com</a></p>
+    <p>Mini Framework planificacion POI V1.1 webapp - <a href="http://localhost/civilweb/dashboard">www.drep.dgi.com</a></p>
 </body>
 </html>
